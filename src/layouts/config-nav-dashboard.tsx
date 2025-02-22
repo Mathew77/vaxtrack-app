63901e4fecb9c89e935945a44a2de349f5875226
Mathew Adegbite <mathew@Mathews-MacBook-Pro.local>
2025-02-22 05:49:44 +0100

    modify headeer to be dynamic with role and menu clean re-arrangement

--- src/layouts/config-nav-dashboard.tsx
+++ src/layouts/config-nav-dashboard.tsx
@@ -8,72 +8,127 @@ const icon = (name: string) => (
 );
 
 export const navData = [
+  // {
+  //   title: 'Dashboard',
+  //   path: '/home',
+  //   icon: icon('ic-analytics'),
+  //   roles: ['admin', 'ehf', 'uhf', 'lcco', 'slwg', 'threepl'],
+  // },
+//EHF Menu
   {
     title: 'Dashboard',
-    path: '/home',
-    icon: icon('ic-analytics'),
-    roles: ['admin', 'ehf', 'uhf', 'lcco', 'slwg', 'threepl'],
-  },
-  {
-    title: 'Vaccine Request Form',
-    path: '/vaccines',
-    icon: icon('ic-user'),
-    roles: ['admin'],
-  },
-  {
-    title: 'EHF Dashboard',
     path: '/ehf-home',
     icon: icon('ic-user'),
-    roles: ['ehf', 'admin'],
+    roles: ['ehf'],
     children: [
       { title: 'EHF Dashboard 1', path: '/ehf-home/dashboard1', roles: ['ehf', 'admin'] },
       { title: 'EHF Dashboard 2', path: '/ehf-home/dashboard2', roles: ['ehf', 'admin'] },
     ],
   },
+  
+  //UHF Menu
   {
-    title: 'UHF Dashboard',
+    title: 'Dashboard',
     path: '/uhf-home',
-    icon: icon('ic-user'),
-    roles: ['uhf', 'admin'],
+    icon: icon('ic-analytics'),
+    roles: ['uhf'],
     children: [
       { title: 'UHF Dashboard 1', path: '/uhf-home/dashboard1', roles: ['uhf', 'admin'] },
       { title: 'UHF Dashboard 2', path: '/uhf-home/dashboard2', roles: ['uhf', 'admin'] },
     ],
   },
+  
+  //LCCO Menu
   {
-    title: 'LCCO Dashboard',
+    title: 'Dashboard',
     path: '/lcco-home',
-    icon: icon('ic-user'),
-    roles: ['lcco', 'admin'],
+    icon: icon('ic-analytics'),
+    roles: ['lcco'],
     children: [
       { title: 'LCCO Dashboard 1', path: '/lcco-home/dashboard1', roles: ['lcco', 'admin'] },
       { title: 'LCCO Dashboard 2', path: '/lcco-home/dashboard2', roles: ['lcco', 'admin'] },
     ],
   },
+  
+  //SLWG Menu
   {
-    title: 'SLWG Dashboard',
+    title: 'Dashboard',
     path: '/slwg-home',
-    icon: icon('ic-user'),
-    roles: ['slwg', 'admin'],
+    icon: icon('ic-analytics'),
+    roles: ['slwg'],
     children: [
       { title: 'SLWG Dashboard 1', path: '/slwg-home/dashboard1', roles: ['slwg', 'admin'] },
       { title: 'SLWG Dashboard 2', path: '/slwg-home/dashboard2', roles: ['slwg', 'admin'] },
     ],
   },
+  
+  //3PL & SCS  Menu
   {
     title: '3PL & SCS Dashboard',
     path: '/threepl-home',
-    icon: icon('ic-user'),
-    roles: ['threepl', 'admin'],
+    icon: icon('ic-analytics'),
+    roles: ['threepl'],
     children: [
       { title: '3PL Dashboard 1', path: '/threepl-home/dashboard1', roles: ['threepl', 'admin'] },
       { title: '3PL Dashboard 2', path: '/threepl-home/dashboard2', roles: ['threepl', 'admin'] },
     ],
   },
+ 
+  //Adminsitratve  Menu
   {
-    title: 'Administrative Dashboard',
+    title: 'Dashboard',
     path: '/admin-home',
+    icon: icon('ic-analytics'),
+    roles: ['admin'],
+  },
+  
+  {
+    title: 'EHF & UHF Setup',
+    path: '/',
     icon: icon('ic-user'),
     roles: ['admin'],
+  },
+  {
+    title: 'LCCO & SLWG SETUP',
+    path: '/',
+    icon: icon('ic-user'),
+    roles: ['admin'],
+  },
+  {
+    title: '3PL & SCS SETUP',
+    path: '/',
+    icon: icon('ic-user'),
+    roles: ['admin'],
+  },
+  {
+    title: 'Roles & Permissions',
+    path: '/',
+    icon: icon('ic-user'),
+    roles: ['admin'],
+  },
+  {
+    title: 'User Management',
+    path: '/',
+    icon: icon('ic-user'),
+    roles: ['admin'],
+  },
+  {
+    title: 'Organisation Unit',
+    path: '/',
+    icon: icon('ic-user'),
+    roles: ['admin'],
+  },
+  //Generic Menu 
+  {
+    title: 'Vaccine Request Form',
+    path: '/vaccines',
+    icon: icon('ic-lock'),
+    roles: ['ehf','uhf','lcco','slwg','threepl'],
+  },
+  {
+    title: 'Report',
+    path: '/report',
+    icon: icon('ic-blog'),
+    roles: ['ehf','uhf','lcco','slwg','threepl'],
   },
 ];
