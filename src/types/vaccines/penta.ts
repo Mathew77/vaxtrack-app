f490ecbca3b1501670118885e9dfab21a5cb76fc
Ddreyy <mradedapodada@gmail.com>
2025-02-24 14:29:15 +0100

    updated on forms and grid lines

---
+++ src/types/vaccines/penta.ts
@@ -0,0 +1,20 @@
+export interface PentaVaccineData {
+    physicalStock: string;
+    avgDailyConsumption: string;
+    dateCreated: string,
+    expiryDate: string;
+    batchNo: string;
+    vvm2: string;
+    numberImmunized: string;
+    daysOfStock: string;
+    adjForAdd: string;
+    belowMinStock: string;
+    aboveMaxStock: string;
+    qtyReceived: string;
+    closingBalance: string;
+    postLmdDos: string;
+  }
+  
+  export interface PentaVaccineProps {
+    onAddToLine: (data: PentaVaccineData) => void
+  }
