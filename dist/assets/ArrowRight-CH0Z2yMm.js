f8fd88a5b8e6044c03081aa796fdaee06268a228
Mathew Adegbite <mathew@Mathews-MacBook-Pro.local>
2025-04-15 07:04:45 +0100

    adding build files

---
+++ dist/assets/ArrowRight-CH0Z2yMm.js
@@ -0,0 +1,1 @@
+import{G as o,j as r}from"./index-jZanNB2p.js";const s=o(r.jsx("path",{d:"m10 17 5-5-5-5z"}),"ArrowRight");export{s as A};
--- dist/assets/ArrowRight-Cl6SS-Oy.js
+++
@@ -1,1 +0,0 @@
-import{G as o,j as r}from"./index-bOWdtkIE.js";const s=o(r.jsx("path",{d:"m10 17 5-5-5-5z"}),"ArrowRight");export{s as A};
