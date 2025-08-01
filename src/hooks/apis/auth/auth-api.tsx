12e733ae2718fe0e71b28a5214f63b273264f147
Mathew Adegbite <mathew@Mathews-MacBook-Pro.local>
2025-08-01 21:19:05 +0100

    build for production

--- src/hooks/apis/auth/auth-api.tsx
+++ src/hooks/apis/auth/auth-api.tsx
@@ -3,7 +3,8 @@
 
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000/api';
+//const BASE_URL = 'http://127.0.0.1:8000/api';
+const BASE_URL = 'https://vaxtracklmd.net/api/';
 
 export interface LoginResponse {
   username: string;
